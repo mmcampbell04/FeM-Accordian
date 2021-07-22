b0bc6027b7ed5892b92342a2b7f2ac418e6889c1
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2021-07-22 20:45:35 +0100

    streamlined functionality

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-// for each accordian item find that button when that button is clicked
-
-
-const accordians = document.querySelectorAll(".accordian-item");
-
-
-
-accordians.forEach(function(accordian) {
-        const btn = accordian.querySelector(".accordian-question");
-        const accordianBodyText = accordian.querySelector(".accordian-body");
-        btn.addEventListener("click", function(){
-            this.classList.toggle("open");
-            accordianBodyText.classList.toggle("open");
-        })
-        
-
+document.querySelectorAll(".accordion-question").forEach((item) => {
+    item.addEventListener("click", (event) => {
+        console.log("click!")
+        item.classList.toggle("open");
+        item.nextElementSibling.classList.toggle("open");
+    })
 });
+
+
+
